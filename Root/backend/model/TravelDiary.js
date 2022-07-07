05d74d53c7f033bd85a8d5918aeda9d00b65fb58
LakminiTharushika <96492@fhss.sjp.ac.lk>
2022-07-07 13:58:21 +0530

    Backend

--- Root/backend/model/TravelDiary.js
+++ Root/backend/model/TravelDiary.js
@@ -2,3 +2,27 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
+const TravelDiarySchema = new Schema ({
+    title: {
+        type: String,
+        required: true 
+    },
+
+    description: {
+        type: String,
+        required: true 
+    },
+
+    image: {
+        type: String,
+        required: true
+    },
+
+    user: {
+        type: String,
+        required: true
+    },
+
+});
+
+export default mongoose.model("TravelDiary", TravelDiarySchema);
