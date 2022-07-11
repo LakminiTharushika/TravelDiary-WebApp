b767b33a61d05ed11b15a13c12dd66a9e2705835
LakminiTharushika <96492@fhss.sjp.ac.lk>
2022-07-11 17:25:41 +0530

    Buttons added.

--- Root/frontend/src/components/Header.js
+++ Root/frontend/src/components/Header.js
@@ -1,9 +1,27 @@
 import React from "react";
+import {
+    AppBar, 
+    Typography, 
+    Toolbar, 
+    Box, 
+    Button} from '@mui/material';
 
-const Login = () => {
+const Header = () => {
     return (
-        <div> Login </div>
-    )
+        <AppBar sx={{background: "linear-gradient( 135deg, #1904E5 10%, #BC78EC 100%);"}}>
+            <Toolbar>
+                <Typography variant="h4"> TravelDiaryApp </Typography>
+                <Box display="flex" marginLeft="auto">
+                    <Button variant="contained" sx={{margin: 1, borderRadius:20}} color="warning"> 
+                    Login </Button>
+                    <Button variant="contained" sx={{margin: 1, borderRadius:20}} color="warning"> 
+                    Signup </Button>
+                </Box>
 
+            </Toolbar>
+        </AppBar>
+
+    );
+    
 }
-export default Login;
+export default Header;
