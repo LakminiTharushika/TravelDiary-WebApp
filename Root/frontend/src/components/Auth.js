97f3ec302484eaffd94aeff75579dd0c95b46320
LakminiTharushika <96492@fhss.sjp.ac.lk>
2022-07-12 03:32:35 +0530

    Make References.

--- Root/frontend/src/components/Auth.js
+++ Root/frontend/src/components/Auth.js
@@ -1,12 +1,42 @@
-import React from "react";
-import {
-    Box, Button, TextField, Typography,
-} from "@mui/material";
+import React, { useState } from "react";
+import {Box, Button, TextField, Typography,} from "@mui/material";
+import axios from "axios";
 
 const Auth = () => {
+    const [inputs, setInputs] = useState({
+        name: "", email:"", password:""
+    });
+
+    const [isSignup, setIsSignup] = useState(false);
+    const handleChange = (e) => {
+        setInputs((prevState)=> ({
+            ...prevState,
+            [e.target.name] : e.target.value
+        }));
+    };
+
+    const sendRequest = async ()=>{
+        const res = await axios.post("http://localhost:3000/api/user/login" ,{
+            email: inputs.email,
+            password: inputs.password
+        }).catch(err=> console.log(err));
+
+        const data = await res.data;
+        return data;
+
+        }
+    
+
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        console.log(inputs);
+        sendRequest();
+    };
+
+
     return (
         <div> 
-            <form> 
+            <form onSubmit={handleSubmit}> 
                 <Box 
                 maxWidth={450}
                 display = "flex" 
@@ -20,19 +50,51 @@ const Auth = () => {
                 borderRadius = {5}
                 bgcolor = "#FFFFF2"
                 >
-                    <Typography variant="h3" padding={3} textAlign="center"> Login </Typography>
-                    <TextField placeholder="Your Name" margin="normal"  />
-                    <TextField type={"email"} placeholder="Your Email Address" margin="normal"  />
-                    <TextField type={"password"}placeholder="Password" margin="normal"  />
+                    <Typography variant="h3" padding={3} textAlign="center"> Login and Signup </Typography>
+                    {isSignup ? "Signup" :"Login"}
 
-                    <Button variant="contained" sx={{borderRadius:5, marginTop:3}} color="warning"> Submit </Button>
-                    <Button sx={{borderRadius:5, marginTop:3}} color="warning"> Change To Signup </Button>
+                    {isSignup && 
+                    <TextField 
+                    name = "name"
+                    onChange = {handleChange} 
+                    value  = {inputs.name} 
+                    placeholder = "Your Name" 
+                    margin = "normal"  />} {" "}
+
+                    <TextField 
+                    name = "email"
+                    onChange = {handleChange}
+                    type = {"email"} 
+                    value = {inputs.email}  
+                    placeholder = "Your Email Address" 
+                    margin = "normal"  />
+
+                    <TextField 
+                    name = "password"
+                    onChange = {handleChange}
+                    type = {"password"} 
+                    value = {inputs.password} 
+                    placeholder = "Password" 
+                    margin = "normal"  />
+
+                    <Button 
+                    type="submit"
+                    variant="contained" 
+                    sx={{borderRadius:5, marginTop:3}} 
+                    color="warning"> Submit Your Details 
+                    </Button>
+
+                    <Button 
+                    onClick={()=>setIsSignup(!isSignup)} 
+                    sx={{borderRadius:5, marginTop:3}} 
+                    color="warning"> Change To {isSignup ? "Login" :"Signup"} 
+                    </Button>
 
 
                 </Box>
             </form>
         </div>
-    )
+    );
 
-}
+};
 export default Auth;
