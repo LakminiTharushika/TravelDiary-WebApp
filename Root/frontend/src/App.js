f384cd5a590b8f64d75d8e04502e7fe52f62387f
LakminiTharushika <96492@fhss.sjp.ac.lk>
2022-07-11 19:56:07 +0530

    Component files linked.

--- Root/frontend/src/App.js
+++ Root/frontend/src/App.js
@@ -1,9 +1,31 @@
 import Header from "./components/Header";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Auth from "./components/Auth";
+import TravelDiaries from "./components/TravelDiaries";
+import UserTravelDiaries from "./components/UserTravelDiaries";
+import DiaryDetails from "./components/DiaryDetails";
+import AddDiary from "./components/AddDiary";
+
 function App() {
-  return <Header />;
+  return <React.Fragment>
+    <header>
+      <Header/>
+    </header>
+
+    <main>
+      <Routes>
+        <Route path = "/auth" element={<Auth/>} />
+        <Route path = "/traveldiaries" element={<TravelDiaries/>} />
+        <Route path = "/myDiaries" element={<UserTravelDiaries/>} />
+        <Route path = "/myDiaries/:id" element={<DiaryDetails/>} />
+        <Route path = "/traveldiaries/add" element={<AddDiary/>} />
+      </Routes>
+    </main>
 
 
-    
+  </React.Fragment>;
+
 }
 
 export default App;
