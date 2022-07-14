93c80ba2d4d8690fe6fc32b721ce6d23ad547902
LakminiTharushika <96492@fhss.sjp.ac.lk>
2022-07-14 17:14:59 +0530

    Card format edited.

--- Root/frontend/src/components/TravelDiary.js
+++ Root/frontend/src/components/TravelDiary.js
@@ -3,7 +3,10 @@ import {Avatar, CardContent, CardMedia, Typography, Card, CardHeader } from "@mu
 
 const TravelDiary = () => {
     return (
-        <div> <Card sx={{ maxWidth: 345 }}>
+        <div> <Card sx={{ width:"40%", margin:'auto',mt:2, padding:2, boxShadow:"10px 10px 20px #ccc", ":hover:" :{
+            boxShadow:"15px 15px 30px #ccc"
+        } }}>
+        
         <CardHeader
           avatar={
             <Avatar sx={{ bgcolor:"red" }} aria-label="recipe">
